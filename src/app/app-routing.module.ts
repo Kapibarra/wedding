40f72b82b49ab8d7455b1704d4cabe6e015a68b5
kapibarra2315 <nikita.nov2016@yandex.ru>
2024-08-09 17:25:35 +0300

    апвыв

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InviteComponent } from './invite/invite.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'guest/:id', component: InviteComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
